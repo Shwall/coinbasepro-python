068ee8bf4bdb7e6c7dc409bbbc38d5eb96ec0183
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2021-08-19 10:49:19 -0400

    Add rate limiting to Messenger class

--- cbpro/messenger.py
+++ cbpro/messenger.py
@@ -1,5 +1,10 @@
 import requests
+import time
+
 import cbpro.auth
+
+
+__sleep__ = 0.275
 
 
 class Messenger(object):
@@ -9,26 +14,23 @@ class Messenger(object):
                  timeout: int = None) -> None:
 
         api = 'https://api.pro.coinbase.com'
-
         self.auth = auth
         self.url = api if url is None else url.rstrip('/')
         self.timeout = 30 if timeout is None else timeout
-
         self.session = requests.Session()
 
     def route(self, endpoint: str) -> str:
         return f'{self.url}{endpoint}'
 
     def get(self, endpoint: str, params: dict = None) -> dict:
+        time.sleep(__sleep__)
         url = self.route(endpoint)
-
         response = self.session.get(
             url,
             params=params,
             auth=self.auth,
             timeout=self.timeout
         )
-
         return response.json()
 
     def post(self,
@@ -36,6 +38,7 @@ class Messenger(object):
              params: dict = None,
              json: dict = None) -> dict:
 
+        time.sleep(__sleep__)
         response = self.session.post(
             url=self.route(endpoint),
             params=params,
@@ -47,15 +50,14 @@ class Messenger(object):
         return response.json()
 
     def delete(self, endpoint: str, **kwargs: dict) -> dict:
+        time.sleep(__sleep__)
         url = self.route(endpoint)
-
         response = self.session.delete(
             url,
             auth=self.auth,
             timeout=self.timeout,
             **kwargs
         )
-
         return response.json()
 
     def request(self,
@@ -64,8 +66,8 @@ class Messenger(object):
                 params: dict = None,
                 json: dict = None) -> dict:
 
+        time.sleep(__sleep__)
         url = self.route(endpoint)
-
         response = self.session.request(
             method,
             url,
@@ -74,39 +76,31 @@ class Messenger(object):
             auth=self.auth,
             timeout=self.timeout
         )
-
         return response.json()
 
     def paginate(self, endpoint: str, params: dict = None) -> object:
         # source: https://docs.pro.coinbase.com/?python#pagination
         url = self.route(endpoint)
-
         if params is None:
             params = dict()
-
         while True:
+            time.sleep(__sleep__)
             response = self.session.get(
                 url,
                 params=params,
                 auth=self.auth,
                 timeout=self.timeout
             )
-
             results = response.json()
-
             if response.status_code != 200:
                 return results
-
             for result in results:
                 yield result
-
             after = response.headers.get('CB-AFTER')
             before = params.get('before')
             end = not after or before
-
             if end:
                 break
-
             params['after'] = response.headers['CB-AFTER']
 
 
